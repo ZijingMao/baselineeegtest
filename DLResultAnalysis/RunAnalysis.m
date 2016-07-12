e668943f878ab039cb5d02b2e623b591606de0c7
ZijingMao <mzj168@hotmail.com>
2016-07-12 09:45:45 -0500

    images

--- DLResultAnalysis/RunAnalysis.m
+++ DLResultAnalysis/RunAnalysis.m
@@ -21,8 +21,9 @@ a = testAUCAll(i, 1:j);
 a = a';
 
 %%
-DLPATH = ['C:\Users\EEGLab\Dropbox\UTSA Research\Collaboration' ...
-    '\EEGRoomPC\Reports\CombineSignalAnalysis'];
+hyper_param_str = 'Lr0.0006';
+
+DLPATH = ['C:\Users\EEGLab\Documents\result\SelectFeat\' hyper_param_str];
 RLTPATH = ['C:\Users\EEGLab\Dropbox\UTSA Research\Collaboration' ...
     '\EEGRoomPC\Reports\CSA_Result'];
 
@@ -30,25 +31,30 @@ result_folders = {'RSVP_X2_S01_NORM_CH64', ...
     'RSVP_X2_S01_RAW_CH64', ...
     'RSVP_X2_S01_RAWFREQ_CH64', ...
     'RSVP_X2_S01_RAWP300_CH64'};
-
-folderIdx = 1;
-
-total_type_size = length(result_folders);
-top16_vals = zeros(16, total_type_size);
-top16_models = cell(16, total_type_size);
-
-for rlt_fld_idx = 1:total_type_size
+folderIdxs = [1, 5, 7, 8];
+for idx = 2:4;
+    folderIdx = folderIdxs(idx);
+    total_type_size = length(result_folders);
+    top16_vals = zeros(16, total_type_size);
+    top16_models = cell(16, total_type_size);
+    best1_vals = zeros(1, total_type_size);
     
-    [ testAUCAll, trainAUCAll, validAUCAll, testAUCMax, testAUCIdx, ...
-        currMaxIdx, setFilesList] = get_one_fold_result...
-        ( DLPATH, result_folders{rlt_fld_idx}, folderIdx );
-    
-    [top16_vals(:, rlt_fld_idx), top16_models(:, rlt_fld_idx)] = ...
-        get_top_16_model(validAUCAll, testAUCAll, setFilesList);
+    for rlt_fld_idx = 1:total_type_size
         
+        [ testAUCAll, trainAUCAll, validAUCAll, testAUCMax, testAUCIdx, ...
+            currMaxIdx, setFilesList] = get_one_fold_result...
+            ( DLPATH, result_folders{rlt_fld_idx}, folderIdx );
+        
+        [i,j]=find(validAUCAll == max(max(validAUCAll)));
+        best1_vals(rlt_fld_idx) = testAUCAll(i,j);
+        
+        [top16_vals(:, rlt_fld_idx), top16_models(:, rlt_fld_idx)] = ...
+            get_top_16_model(validAUCAll, testAUCAll, setFilesList);
+        
+    end
+    save([RLTPATH '\model' num2str(folderIdx) hyper_param_str '.mat'],...
+        'top16_models', 'top16_vals', 'best1_vals');
 end
-save([RLTPATH '\model' num2str(folderIdx) '.mat'], 'top16_models', 'top16_vals');
-
 %%
 [h_norm_vs_raw, p_norm_vs_raw] = ttest(top16_vals(:, 1), top16_vals(:, 2));
 [h_raw_vs_rawp300, p_raw_vs_rawp300] = ttest(top16_vals(:, 2), top16_vals(:, 4));
--- VisualizationFilters/Scripts/RunMe.m
+++ VisualizationFilters/Scripts/RunMe.m
@@ -30,3 +30,90 @@ vis_multiple_frames(curr_weights_raw, curr_weights_res, handles);
 % run this code to have quick test if you want
 % handles.visible = true;
 % generate_frames(curr_weights, handles);
+
+%% plot imagesc
+p = cell(3, 3);
+for i = 1:3
+for j = 1:3
+h((j-1)*3+i)=subplot(3, 3, (j-1)*3+i);
+p{(j-1)*3+i} = get(h((j-1)*3+i), 'pos');
+colormap(jet);imagesc(squeeze(input_data(1:64, 1:64, 1, (j-1)*3+i)));caxis([-0.2, 1.2]);
+end
+end
+
+%%
+% [ha, pos] = tight_subplot(1,2,[.01 .01]) ;
+load('neg.mat')
+idx = [ 1, 3, 4, 6, 7];
+c = [];
+for i=1:length(idx)
+b = padarray(squeeze(input_data(1:64, 1:64, 1, idx(i))), [1, 1], -1);
+c = [c; b];
+end
+createfigure(c, 'neg_input.png');
+
+c = [];
+for i=1:length(idx)
+b = padarray(squeeze(recon_data(1:64, 1:64, 1, idx(i))), [1, 1], -1);
+c = [c; b];
+end
+createfigure_rec(c, 'neg_recon.png');
+
+%%
+% [ha, pos] = tight_subplot(1,2,[.01 .01]) ;
+load('pos.mat')
+idx = [ 3, 4, 5, 7, 9];
+c = [];
+for i=1:length(idx)
+b = padarray(squeeze(input_data(1:64, 1:64, 1, idx(i))), [1, 1], -1);
+c = [c; b];
+end
+createfigure(c, 'pos_input.png');
+
+c = [];
+for i=1:length(idx)
+b = padarray(squeeze(recon_data(1:64, 1:64, 1, idx(i))), [1, 1], -1);
+c = [c; b];
+end
+createfigure_rec(c, 'pos_recon.png');
+
+%%
+load('neg.mat')
+a = squeeze(recon_data(1:64, 1:64, 1, 7));
+
+% load('s01_erp.mat')
+
+handles.visible = false;
+handles.setcaxis = true;
+handles.chanlocs = chanlocs64;
+handles.min_val = -0.5;	% change the min value for the color
+handles.max_val = 1.2;	% change the max value for the color
+handles.mycmap = set_mycmap();	% Initialize colormap matrix
+chanlocs64 = [];
+load('C:\Users\EEGLab\Dropbox\UTSA Research\Collaboration\EEGRoomPC\Zijing Mao\baselineeegtest\Source\ChannelSrc\chanlocs_bk.mat', 'chanlocs64')
+handles.chanlocs = chanlocs64;
+handles.saveFig = false;
+
+currTime = 15;
+handles.child_fig = subplot(1, 2, 1);
+generateBrainmap(currTime/64, a, currTime, handles);
+load('pos.mat')
+a = squeeze(recon_data(1:64, 1:64, 1, 9));
+handles.child_fig = subplot(1, 2, 2);
+generateBrainmap(currTime/64, a, currTime, handles);
+
+currTime = 19;
+handles.child_fig = subplot(1, 2, 1);
+generateBrainmap(currTime/64, rawnon, currTime, handles);
+handles.child_fig = subplot(1, 2, 2);
+generateBrainmap(currTime/64, rawtar, currTime, handles);
+
+%%
+
+load('neg.mat')
+curr_weights_neg = squeeze(recon_data(1:64, 1:64, 1, 7));
+
+load('pos.mat')
+curr_weights_pos = squeeze(recon_data(1:64, 1:64, 1, 9));
+
+vis_multiple_frames_adapt(curr_weights_neg,curr_weights_pos, handles);
