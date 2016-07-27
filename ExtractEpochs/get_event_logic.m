f616bf9aca6398527a348bc47d0f4b5af8aedbaa
Ehren Biglari <ebiglari@gmail.com>
2016-07-27 03:36:41 -0500

    Added epoch extraction code for detection of target/non-target for RSP and Guard Duty data sets

---
+++ ExtractEpochs/get_event_logic.m
@@ -0,0 +1,10 @@
+function [ non_zero_event ] = get_event_logic( eventtype, event )
+
+mattype_logic = zeros(1, length(eventtype));
+for epoch_event_idx = 1:length(event)
+    [ mattype_tmp ] = event_strcmp( eventtype, event{epoch_event_idx} );
+    mattype_logic = mattype_logic | mattype_tmp;
+end
+non_zero_event = find(mattype_logic);
+
+end
